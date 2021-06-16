734ecc04281fbdbc90d1416550e700996934c9f3
TomaszKolodziejczak <64540133+TomaszKolodziejczak@users.noreply.github.com>
2021-06-16 15:13:12 +0200

    refactor testing

---
+++ testing/test_functions.py
@@ -0,0 +1,14 @@
+import pytest
+
+from functions import is_palindrom
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    ('ala', True),
+    ('level  ', True),
+    ('LEVel,', True),
+    ('Cat', True),
+
+])
+def test_is_palindrom(test_input, expected):
+    assert is_palindrom(test_input) == expected
