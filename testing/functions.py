00bf8b69d1fa0e1f72693b70849dc19521726554
TomaszKolodziejczak <64540133+TomaszKolodziejczak@users.noreply.github.com>
2021-06-17 13:38:09 +0200

    another func test

--- testing/functions.py
+++ testing/functions.py
@@ -1,4 +1,17 @@
+from textwrap import wrap
+from typing import Union, List, Tuple
+
+
 def is_palindrom(text: str) -> bool:
     # this function returns True if string is palindrom or False if it's not
     text = text.lower().replace(',', '').strip()
     return text == text[::-1]
+
+
+def format_phone_number(number: str, area_code: str = '+48', delimeter: str = '-') -> str:
+    wrapped_number = delimeter.join(wrap(number, 3))
+    return f"{area_code} {wrapped_number}"
+
+
+if __name__ == "__main__":
+    print(format_phone_number('666777888'))
--- testing/test_functions.py
+++ testing/test_functions.py
@@ -1,5 +1,5 @@
 import pytest
-from functions import is_palindrom
+from functions import is_palindrom, format_phone_number
 
 
 @pytest.mark.parametrize("test_input,expected", [
@@ -11,3 +11,12 @@ from functions import is_palindrom
 ])
 def test_is_palindrom(test_input, expected):
     assert is_palindrom(test_input) == expected
+
+
+def test_format_phone_number():
+    data = {
+        'delimeter': '_',
+        'number': '444555666',
+        'area_code': '+49',
+    }
+    assert format_phone_number(**data) == '+49 444_555_666'
