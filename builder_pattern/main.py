331b0ca6bac87d79f3140aa3afa53ba514f2abc9
TomaszKolodziejczak <64540133+TomaszKolodziejczak@users.noreply.github.com>
2021-06-25 10:11:03 +0200

    builder pattern c4

--- builder_pattern/main.py
+++ builder_pattern/main.py
@@ -2,12 +2,14 @@ from builder_pattern.a_computer_builder import AComputerBuilder
 from builder_pattern.b_computer_builder import BComputerBuilder
 from builder_pattern.director import Director
 
+# computer ver 1
 builder_1 = AComputerBuilder()
 d = Director(builder_1)
 d.build_computer()
 computer = d.get_computer()
 computer.display()
 
+# computer ver 2
 builder_2 = BComputerBuilder()
 d = Director(builder_2)
 d.build_computer()
