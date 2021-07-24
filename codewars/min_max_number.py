182f1dc1ba3f6c759f8d39b4165711e080038b06
tomasz <kolodziejczak.tomasz@gmail.com>
2021-07-24 12:25:54 +0200

    min_max_number

---
+++ codewars/min_max_number.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def min_max_number(nums: List) -> str:
+    minimum, maximum = None, None
+
+    for num in nums:
+        if not maximum or maximum < num:
+            maximum = num
+
+        if not minimum or minimum > num:
+            minimum = num
+
+    return f'minimum: {minimum}\nmaximum: {maximum}'
+
+
+if __name__ == "__main__":
+    nums = [17, -4, 6, 9, 12, -334, -2, 1]
+    print(min_max_number(nums))
