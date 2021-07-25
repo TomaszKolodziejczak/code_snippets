92feca11d7c5a620eef592fcdac419d155869ed3
tomasz <kolodziejczak.tomasz@gmail.com>
2021-07-25 20:29:43 +0200

    refactoring

--- decorators_2.py
+++ decorators_2.py
@@ -5,7 +5,6 @@ def loving_python(fn):
     def wrapper(*args, **kwargs):
         print('No matter what the function returns, let everyone know You love Python!')
         return fn(*args, **kwargs)
-
     return wrapper
 
 
@@ -13,7 +12,6 @@ def capitalise_text(fn):
     def wrapper(*args):
         result = fn(*args)
         return result.title()
-
     return wrapper
 
 
@@ -23,18 +21,26 @@ def run_only_between(from_, to_):
             if from_ <= datetime.now().hour < to_:
                 return fn(*args, **kwargs)
         return wrapper
-
     return dec
 
 
 @capitalise_text
-@loving_python
-@run_only_between(6, 15)
 def introduce_yourself(name, age):
     return f'My name is {name} and I am {age} yo.'
 
 
+@run_only_between(6, 22)
+def say_something():
+    return 'Hello World!'
+
+
+@loving_python
+def good_luck():
+    return 'Good luck!'
+
+
 if __name__ == "__main__":
     print(introduce_yourself('tom', 19))
-
+    print(say_something())
+    print(good_luck())
 
